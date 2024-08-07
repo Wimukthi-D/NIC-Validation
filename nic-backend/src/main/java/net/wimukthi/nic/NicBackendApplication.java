6c18c1c2fd8f94665f87955f1783b032df1300ae
Wimukthi-D <160035491+Wimukthi-D@users.noreply.github.com>
2024-08-08 00:56:59 +0530

    initiation

---
+++ nic-backend/src/main/java/net/wimukthi/nic/NicBackendApplication.java
@@ -0,0 +1,13 @@
+package net.wimukthi.nic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicBackendApplication.class, args);
+	}
+
+}
---
+++ nic-backend/src/test/java/net/wimukthi/nic/NicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wimukthi.nic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
