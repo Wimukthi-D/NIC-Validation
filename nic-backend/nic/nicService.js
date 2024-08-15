c49a16aaf528190858edcf280b6770ad24fdbec8
Wimukthi-D <160035491+Wimukthi-D@users.noreply.github.com>
2024-08-15 17:01:47 +0530

    8-15-2

--- nic-backend/nic/nicService.js
+++ nic-backend/nic/nicService.js
@@ -3,56 +3,63 @@ const app = express();
 const PORT = 3003;
 const connection = require("./nicdb");
 app.use(express.json());
+const dayjs = require("dayjs");
 
-app.post("/save", (req, res) => {
-  const Numbers = req.body;
-  console.log(Numbers);
+const extractNicInfo = (nic) => {
+  if (!nic || (nic.length !== 10 && nic.length !== 12)) {
+    return { birthday: "Invalid NIC", gender: "N/A", age: "N/A" };
+  }
 
-  Numbers.forEach((Number) => {
-    const nic = Number.nic;
-    const birthday = Number.birthday;
-    const gender = Number.gender;
-    const age = Number.age;
+  let birthYear, dayOfYear, gender, age;
+  const currentYear = dayjs().year();
 
-    connection.query(
-      "SELECT * FROM records WHERE nic = ?",
-      [nic],
-      (error, results) => {
-        if (error) {
-          console.log("An error occured:", error);
-          return res.status(500).send("An error occured: " + error);
-        }
+  if (nic.length === 10) {
+    birthYear = parseInt(nic.substring(0, 2), 10) + 1900;
+    dayOfYear = parseInt(nic.substring(2, 5), 10);
+    gender = dayOfYear > 500 ? "Female" : "Male";
+    dayOfYear = dayOfYear > 500 ? dayOfYear - 500 : dayOfYear;
+  } else if (nic.length === 12) {
+    birthYear = parseInt(nic.substring(0, 4), 10);
+    dayOfYear = parseInt(nic.substring(4, 7), 10);
+    gender = dayOfYear > 500 ? "Female" : "Male";
+    dayOfYear = dayOfYear > 500 ? dayOfYear - 500 : dayOfYear;
+  }
 
-        if (results && results.length > 0) {
-          connection.query(
-            "UPDATE records SET birthday = ?, gender = ?, age = ? WHERE nic = ?",
-            [birthday, gender, age, nic],
-            (error, results) => {
-              if (error) {
-                console.log("Error in UPDATE query:", error);
-              } else {
-                console.log("Record updated successfully");
-              }
-            }
-          );
-        } else {
-          connection.query(
-            "INSERT INTO records (nic, birthday, gender, age) VALUES (?, ?, ?, ?)",
-            [nic, birthday, gender, age],
-            (error, results) => {
-              if (error) {
-                console.log("Error in INSERT query:", error);
-              } else {
-                console.log("Record inserted successfully");
-              }
-            }
-          );
-        }
-      }
-    );
+  const birthDate = dayjs(`${birthYear}-01-01`).add(dayOfYear - 1, "day");
+  age = currentYear - birthDate.year();
+
+  return {
+    birthday: birthDate.format("YYYY-MM-DD"),
+    gender,
+    age,
+  };
+};
+
+app.post("/validate", (req, res) => {
+  const data = req.body;
+
+  const result = data.map((fileData) => {
+    const { fileName, data: records } = fileData;
+
+    const validatedRecords = records.map((record) => {
+      const { nic, ...otherFields } = record;
+      const nicInfo = extractNicInfo(nic);
+
+      return {
+        ...otherFields,
+        nic,
+        ...nicInfo,
+      };
+    });
+
+    return {
+      fileName,
+      records: validatedRecords,
+    };
   });
+  console.log(result);
 
-  res.send("Data saved successfully");
+  res.status(200).json({ result });
 });
 
 app.get("/getRecordCount", (req, res) => {
--- nic-frontend/src/Pages/Upload.jsx
+++ nic-frontend/src/Pages/Upload.jsx
@@ -19,85 +19,48 @@ import Swal from "sweetalert2";
 
 function Upload() {
   const [files, setFiles] = useState([]);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
-  const handleFileChange = (event, index) => {
-    const selectedFile = event.target.files[0];
-    const updatedFiles = [...files];
-    updatedFiles[index] = selectedFile;
-    setFiles(updatedFiles);
+  const handleFileChange = (event) => {
+    const selectedFiles = Array.from(event.target.files);
+
+    selectedFiles.forEach((file) => {
+      Papa.parse(file, {
+        complete: (result) => {
+          setData((prevData) => ({
+            ...prevData,
+            [file.name]: result.data,
+          }));
+        },
+        header: true,
+      });
+    });
+
+    setFiles([...files, ...selectedFiles]);
   };
 
   const handleRemoveFile = (index) => {
     const updatedFiles = files.filter((_, i) => i !== index);
     setFiles(updatedFiles);
-    const updatedData = data.filter((_, i) => i !== index);
+
+    const fileName = files[index].name;
+    const updatedData = { ...data };
+    delete updatedData[fileName];
     setData(updatedData);
   };
 
-  const extractNicInfo = (nic) => {
-    if (!nic || (nic.length !== 10 && nic.length !== 12)) {
-      return { birthday: "Invalid NIC", gender: "N/A", age: "N/A" };
-    }
-
-    let birthYear, dayOfYear, gender, age;
-    const currentYear = dayjs().year();
-
-    if (nic.length === 10) {
-      // Old format:
-      birthYear = parseInt(nic.substring(0, 2), 10) + 1900;
-      dayOfYear = parseInt(nic.substring(2, 5), 10);
-      gender = dayOfYear > 500 ? "Female" : "Male";
-      dayOfYear = dayOfYear > 500 ? dayOfYear - 500 : dayOfYear;
-    } else if (nic.length === 12) {
-      // New format:
-      birthYear = parseInt(nic.substring(0, 4), 10);
-      dayOfYear = parseInt(nic.substring(4, 7), 10);
-      gender = dayOfYear > 500 ? "Female" : "Male";
-      dayOfYear = dayOfYear > 500 ? dayOfYear - 500 : dayOfYear;
-    }
-
-    const birthDate = dayjs(`${birthYear}-01-01`).add(dayOfYear - 1, "day");
-    age = currentYear - birthDate.year();
-
-    return {
-      birthday: birthDate.format("YYYY-MM-DD"),
-      gender,
-      age,
-    };
-  };
-
   const handleUpload = () => {
-    let combinedData = [];
+    const combinedData = files.map((file) => ({
+      fileName: file.name,
+      data: data[file.name],
+    }));
 
-    files.forEach((file, fileIndex) => {
-      Papa.parse(file, {
-        header: false,
-        complete: (result) => {
-          const extractedData = result.data.map((row) => {
-            const nic = row[0];
-            const { birthday, gender, age } = extractNicInfo(nic);
-            return { nic, birthday, gender, age };
-          });
-
-          combinedData = [...combinedData, ...extractedData];
-
-          // Only update the state after the last file is processed
-          if (fileIndex === files.length - 1) {
-            setData(combinedData);
-          }
-        },
-      });
-    });
-  };
-
-  console.log(data);
-
-  const handleSubmit = () => {
     try {
-      axios.post("http://localhost:3001/nic/save", data).then((response) => {
-        console.log(response);
-      });
+      axios
+        .post("http://localhost:3001/nic/validate", combinedData)
+        .then((response) => {
+          console.log(response);
+        });
 
       Swal.fire({
         icon: "success",
@@ -106,7 +69,7 @@ function Upload() {
         timer: 1500,
       });
 
-      setData([]);
+      setData({});
       setFiles([]);
     } catch (error) {
       console.log(error);
@@ -123,7 +86,7 @@ function Upload() {
     <div className="flex flex-col w-screen h-screen">
       <Navbar />
       <div className="flex w-full h-full">
-        <div className="flex flex-col w-1/3 h-full p-4 space-y-4">
+        <div className="flex flex-col w-1/3 items-center h-full p-4 space-y-4">
           {files.map((file, index) => (
             <div key={index} className="flex items-center space-x-2">
               <input
@@ -152,80 +115,37 @@ function Upload() {
               )}
             </div>
           ))}
-          {files.length < 4 && (
-            <div className="flex items-center space-x-2">
-              <input
-                accept=".csv"
-                style={{ display: "none" }}
-                id={`raised-button-file-${files.length}`}
-                type="file"
-                onChange={(e) => handleFileChange(e, files.length)}
-              />
-              <label htmlFor={`raised-button-file-${files.length}`}>
-                <Button
-                  variant="contained"
-                  component="span"
-                  startIcon={<UploadFile />}
-                >
-                  Upload CSV File
-                </Button>
-              </label>
-            </div>
-          )}
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleUpload}
-            disabled={files.length === 0}
-          >
-            Submit
-          </Button>
-        </div>
-        <div className="flex flex-col w-2/3  h-full p-4">
-          <div className="flex flex-col h-full">
-            <div className="flex justify-end mr-10 mb-4 gap-4">
+          <div className="flex items-center space-x-2">
+            <input
+              accept=".csv"
+              style={{ display: "none" }}
+              id="raised-button-file"
+              type="file"
+              onChange={handleFileChange}
+              multiple
+            />
+            <label htmlFor="raised-button-file">
               <Button
                 variant="contained"
-                color="error"
-                onClick={() => setData([])}
+                component="span"
+                startIcon={<UploadFile />}
               >
-                Clear
+                Upload CSV Files
               </Button>
-              <Button
-                variant="contained"
-                color="success"
-                onClick={handleSubmit}
-              >
-                Submit
-              </Button>
-            </div>
-            <TableContainer
-              component={Paper}
-              className="flex h-full w-full border"
+            </label>
+          </div>
+          <div className="flex w1/2">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleUpload}
+              disabled={files.length === 0}
             >
-              <Table aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>NIC</TableCell>
-                    <TableCell>Birthday</TableCell>
-                    <TableCell>Gender</TableCell>
-                    <TableCell>Age</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((row, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{row.nic}</TableCell>
-                      <TableCell>{row.birthday}</TableCell>
-                      <TableCell>{row.gender}</TableCell>
-                      <TableCell>{row.age}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+              Submit
+            </Button>
           </div>
         </div>
+        <div className="flex flex-col w-2/3 h-full p-4"></div>
       </div>
     </div>
   );
